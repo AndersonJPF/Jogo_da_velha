6b04351e8a0b9bd989ad9c3ad439ab1f3ef04ba4
Anderson <andersonjpf97@gmail.com>
2022-04-22 12:35:10 -0300

    Parte 3 - paleta de cores, nova fonte, botao de reiniciar

--- jogo.js
+++ jogo.js
@@ -1,5 +1,41 @@
 let jogador = null
+let jogadorSelecionado = document.getElementById('jogador-selecionado')
+let quadrados = document.getElementsByClassName('quadrado')
+
+MudarJogador('X')
 
 function SelecionarQuadrado (id) {
-    console.log(id)
+    let quadrado = document.getElementById(id)
+
+    if (quadrado.innerHTML !== '-') {
+        return
+    }
+
+    quadrado.innerHTML = jogador
+    quadrado.classList.add('player' + jogador)
+    console.log('player' + jogador)
+
+    if (jogador === 'X') {
+        jogador = 'O'
+    }
+    else {
+        jogador = 'X'
+    }
+    MudarJogador(jogador)
+}
+
+function MudarJogador(valor) {
+    jogador = valor
+    jogadorSelecionado.innerHTML = valor
+    jogadorSelecionado.classList.remove('playerX', 'playerO')
+    jogadorSelecionado.classList.add('player' + jogador)
+}
+
+function reiniciar() {
+    Array.from(quadrados).forEach(element => {
+        element.innerHTML = '-'
+        element.classList.remove('playerX')
+        element.classList.remove('playerO')
+    MudarJogador('X')
+    });
 }
